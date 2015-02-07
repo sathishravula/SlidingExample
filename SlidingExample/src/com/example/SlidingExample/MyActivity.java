9df2609780ea82535c8ba7b5dffeb48abe17aa69
Sathish Ravula <sathish@eggheadcreative.com>
2015-02-08 02:30:04 +0530

    done

--- SlidingExample/src/com/example/SlidingExample/MyActivity.java
+++ SlidingExample/src/com/example/SlidingExample/MyActivity.java
@@ -1,15 +1,166 @@
 package com.example.SlidingExample;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MyActivity extends Activity {
-  /**
-   * Called when the activity is first created.
-   */
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
-  }
+public class MyActivity extends Activity implements View.OnClickListener {
+    private TextView singin;
+    private LinearLayout singUp;
+    private LinearLayout singIn;
+    private TextView email;
+    private TextView password;
+    private TextView singInCountinue;
+    private TextView singUpCountinue;
+    private TextView lname, fname;
+    private SharedPreferences session;
+    private SharedPreferences.Editor editor;
+    private String emailId;
+    private String PassWord;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        singUp = (LinearLayout) findViewById(R.id.singup);
+        singIn = (LinearLayout) findViewById(R.id.singin);
+        singIn.setOnClickListener(this);
+        singUp.setOnClickListener(this);
+        session = getSharedPreferences("session", Context.MODE_MULTI_PROCESS);
+        editor = session.edit();
+        if(session.contains("ok")){
+            startActivity();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.singin:
+                final Dialog dialog = new Dialog(this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.sing_in);
+//                dialog.setTitle("Sing In");
+//                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#99000000")));
+                email = (TextView) dialog.findViewById(R.id.email);
+                password = (TextView) dialog.findViewById(R.id.password);
+                singInCountinue = (TextView) dialog.findViewById(R.id.singin_continue);
+                singInCountinue.setOnClickListener(this);
+                dialog.show();
+                break;
+            case R.id.singup:
+                final Dialog dialog1 = new Dialog(this);
+                dialog1.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog1.setContentView(R.layout.sing_up);
+//                dialog1.setTitle("Sing Up");
+                email = (TextView) dialog1.findViewById(R.id.email);
+                lname = (TextView) dialog1.findViewById(R.id.lname);
+                fname = (TextView) dialog1.findViewById(R.id.fname);
+                password = (TextView) dialog1.findViewById(R.id.password);
+                singUpCountinue = (TextView) dialog1.findViewById(R.id.singup_continue);
+                singUpCountinue.setOnClickListener(this);
+                dialog1.show();
+                break;
+            case R.id.singup_continue:
+                if (doValidation1()) {
+                    editor.putString("ok", "ok").commit();
+                    editor.putString("email", emailId).commit();
+                    editor.putString("password", PassWord).commit();
+                    startActivity();
+                }
+                break;
+
+            case R.id.singin_continue:
+                if (doValidation()) {
+                    if (emailId.equalsIgnoreCase(session.getString("email", "")) && PassWord.equalsIgnoreCase(session.getString("password", ""))) {
+                        startActivity();
+                    } else {
+                        email.setError("Invalid email");
+                        password.setError("Invalid password");
+                    }
+                }
+                break;
+        }
+    }
+
+    private void startActivity() {
+        Intent intent = new Intent(this, ResponsiveUIActivity.class);
+        startActivity(intent);
+    }
+
+    private boolean doValidation() {
+        String password = this.password.getText().toString().trim();
+        String email = this.email.getText().toString().trim();
+        emailId = email;
+        PassWord = password;
+        if (email.equalsIgnoreCase("")) {
+            this.email.setError("Invalid email id");
+            this.email.requestFocus();
+            return false;
+        }
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() && !TextUtils.isEmpty(email)) {
+            this.email.setError("Invalid email id");
+            this.email.requestFocus();
+            return false;
+        }
+        if (password.equalsIgnoreCase("")) {
+            this.password.setError("Invalid Password");
+            return false;
+        }
+        if (password.length() < 4) {
+            this.password.setError("Length should be greater than 4");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean doValidation1() {
+        String password = this.password.getText().toString().trim();
+        String email = this.email.getText().toString().trim();
+        String lname = this.lname.getText().toString().trim();
+        String fname = this.fname.getText().toString().trim();
+        emailId = email;
+        PassWord = password;
+        if (email.equalsIgnoreCase("")) {
+            this.email.setError("Invalid email id");
+            this.email.requestFocus();
+            return false;
+        }
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() && !TextUtils.isEmpty(email)) {
+            this.email.setError("Invalid email id");
+            this.email.requestFocus();
+            return false;
+        }
+        if (password.equalsIgnoreCase("")) {
+            this.password.setError("Invalid Password");
+            return false;
+        }
+        if (password.length() < 4) {
+            this.password.setError("Length should be greater than 4");
+            return false;
+        }
+
+        if (lname.equalsIgnoreCase("")) {
+            this.lname.setError("Invalid Password");
+            return false;
+        }
+        if (fname.equalsIgnoreCase("")) {
+            this.fname.setError("Invalid Password");
+            return false;
+        }
+        return true;
+    }
 }
--- SlidingExample/src/com/example/SlidingExample/ResponsiveUIActivity.java
+++ SlidingExample/src/com/example/SlidingExample/ResponsiveUIActivity.java
@@ -1,11 +1,14 @@
 package com.example.SlidingExample;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.View;
+import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
@@ -17,102 +20,120 @@ import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
  * On tablets, it will will do the same general thing. In portrait
  * mode, it will enable the SlidingMenu, and in landscape mode, it
  * will be a dual pane layout.
- * 
- * @author jeremy
  *
+ * @author jeremy
  */
 public class ResponsiveUIActivity extends SlidingFragmentActivity {
 
-	private Fragment mContent;
+    private Fragment mContent;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setTitle(R.string.responsive_ui);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("London");
 
-		setContentView(R.layout.responsive_content_frame);
+        setContentView(R.layout.responsive_content_frame);
 
-		// check if the content frame contains the menu frame
-		if (findViewById(R.id.menu_frame) == null) {
-			setBehindContentView(R.layout.menu_frame);
-			getSlidingMenu().setSlidingEnabled(true);
-			getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
-			// show home as up so we can toggle
+        // check if the content frame contains the menu frame
+        if (findViewById(R.id.menu_frame) == null) {
+            setBehindContentView(R.layout.menu_frame);
+            getSlidingMenu().setSlidingEnabled(true);
+            getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+            // show home as up so we can toggle
 			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		} else {
-			// add a dummy view
-			View v = new View(this);
-			setBehindContentView(v);
-			getSlidingMenu().setSlidingEnabled(false);
-			getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
-		}
+        } else {
+            // add a dummy view
+            View v = new View(this);
+            setBehindContentView(v);
+            getSlidingMenu().setSlidingEnabled(false);
+            getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+        }
 
-		// set the Above View Fragment
-		if (savedInstanceState != null)
-			mContent = getSupportFragmentManager().getFragment(savedInstanceState, "mContent");
-		if (mContent == null)
-			mContent = new BirdGridFragment(0);	
-		getSupportFragmentManager()
-		.beginTransaction()
-		.replace(R.id.content_frame, mContent)
-		.commit();
+        // set the Above View Fragment
+        if (savedInstanceState != null)
+            mContent = getSupportFragmentManager().getFragment(savedInstanceState, "mContent");
+        if (mContent == null)
+            mContent = new BirdGridFragment(0);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame, mContent)
+                .commit();
 
-		// set the Behind View Fragment
-		getSupportFragmentManager()
-		.beginTransaction()
-		.replace(R.id.menu_frame, new BirdMenuFragment())
-		.commit();
+        // set the Behind View Fragment
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.menu_frame, new BirdMenuFragment())
+                .commit();
 
-		// customize the SlidingMenu
-		SlidingMenu sm = getSlidingMenu();
-		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-		sm.setShadowWidthRes(R.dimen.shadow_width);
-		sm.setShadowDrawable(R.drawable.shadow);
-		sm.setBehindScrollScale(0.25f);
-		sm.setFadeDegree(0.25f);
+        // customize the SlidingMenu
+        SlidingMenu sm = getSlidingMenu();
+        sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        sm.setShadowWidthRes(R.dimen.shadow_width);
+        sm.setShadowDrawable(R.drawable.shadow);
+        sm.setBehindScrollScale(0.25f);
+        sm.setFadeDegree(0.25f);
 
-		// show the explanation dialog
-	/*	if (savedInstanceState == null)
+        // show the explanation dialog
+    /*	if (savedInstanceState == null)
 			new AlertDialog.Builder(this)
 			.setTitle(R.string.what_is_this)
 			.setMessage(R.string.responsive_explanation)
 			.show();*/
-	}
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			toggle();
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                toggle();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		getSupportFragmentManager().putFragment(outState, "mContent", mContent);
-	}
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        getSupportFragmentManager().putFragment(outState, "mContent", mContent);
+    }
 
-	public void switchContent(final Fragment fragment) {
-		mContent = fragment;
-		getSupportFragmentManager()
-		.beginTransaction()
-		.replace(R.id.content_frame, fragment)
-		.commit();
-		Handler h = new Handler();
-		h.postDelayed(new Runnable() {
-			public void run() {
-				getSlidingMenu().showContent();
-			}
-		}, 50);
-	}	
+    public void switchContent(final Fragment fragment) {
+        mContent = fragment;
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            public void run() {
+                getSlidingMenu().showContent();
+            }
+        }, 50);
+    }
 
-	public void onBirdPressed(int pos) {
-		Intent intent = BirdActivity.newInstance(ResponsiveUIActivity.this, pos);
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                SharedPreferences session = getSharedPreferences("session", Context.MODE_MULTI_PROCESS);
+                session.edit().clear().commit();
+                finish();
+                onBackPressed();
+                break;
+        }
+        return super.onMenuItemSelected(featureId, item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater().inflate(R.menu.logout, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void onBirdPressed(int pos) {
+        Intent intent = BirdActivity.newInstance(ResponsiveUIActivity.this, pos);
 //	Intent intent=new Intent(ResponsiveUIActivity.this,BirdActivity.class);
-			startActivity(intent);
+        startActivity(intent);
 
-	}
+    }
 
 }
